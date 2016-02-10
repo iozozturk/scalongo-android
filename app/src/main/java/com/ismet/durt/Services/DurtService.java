1ac955b277a41a45bb58634467cf2ca5bf74c48c
ismet <iozozturk@gmail.com>
2016-02-10 19:12:16 +0200

    logging enhancements and timeout increased to 1m

--- app/src/main/java/com/ismet/durt/Services/DurtService.java
+++ app/src/main/java/com/ismet/durt/Services/DurtService.java
@@ -2,21 +2,22 @@ package com.ismet.durt.services;
 
 //import com.facebook.stetho.okhttp3.StethoInterceptor;
 
+import com.ismet.durt.models.responses.GenericResponse;
 import com.ismet.durt.models.responses.LoginResponse;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.ResponseBody;
+import okio.Buffer;
 import retrofit2.Call;
 import retrofit2.JacksonConverterFactory;
 import retrofit2.Retrofit;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 /**
@@ -27,12 +28,14 @@ public class DurtService {
     public static Logger logger = Logger.getLogger(DurtService.class.getSimpleName());
 
     public interface Services {
-        String json = "Content-Type: application/json";
 
         @FormUrlEncoded
-        @Headers("Content-Type: application/json")
         @POST("login")
         Call<LoginResponse> login(@Field("username") String username, @Field("password") String password);
+
+        @FormUrlEncoded
+        @POST("signup")
+        Call<GenericResponse> signup(@Field("name") String name, @Field("username") String username, @Field("email") String email, @Field("password") String password);
     }
 
     class LoggingInterceptor implements Interceptor {
@@ -41,14 +44,22 @@ public class DurtService {
             Request request = chain.request();
 
             long t1 = System.nanoTime();
+            Buffer buffer = new Buffer();
+            request.body().writeTo(buffer);
+            String body = buffer.readUtf8();
+
+            request = request.newBuilder()
+                    .addHeader("Content-Type", "application/json")
+                    .build();
+
             logger.info(String.format("Sending request %s on %s%n%s",
-                    request.url(), chain.connection(), request.headers()));
+                    request.url(), body, request.headers()));
 
             okhttp3.Response response = chain.proceed(request);
 
             long t2 = System.nanoTime();
             logger.info(String.format("Received response for %s in %.1fms%n%s",
-                    response.request().url(), (t2 - t1) / 1e6d, response.headers()));
+                    response.request().url(), (t2 - t1) / 1e6d, response.code()));
 
             return response;
         }
@@ -57,6 +68,7 @@ public class DurtService {
     private OkHttpClient client = new OkHttpClient.Builder()
             .addInterceptor(new LoggingInterceptor())
 //            .addInterceptor(new StethoInterceptor())
+            .connectTimeout(1, TimeUnit.MINUTES)
             .build();
 
     private final Retrofit retrofit = new Retrofit.Builder()
