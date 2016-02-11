af5a504b965dd02c24446287520bd316fc685541
ismet <iozozturk@gmail.com>
2016-02-11 20:45:23 +0200

    Changed readme

---
+++ app/src/main/java/com/ismet/durt/mModels/responses/GenericResponse.java
@@ -0,0 +1,8 @@
+package com.ismet.durt.mmodels.responses;
+
+/**
+ * Created by ismet on 09/02/16.
+ */
+public class GenericResponse {
+
+}
---
+++ app/src/main/java/com/ismet/durt/mModels/responses/LoginResponse.java
@@ -0,0 +1,17 @@
+package com.ismet.durt.mmodels.responses;
+
+/**
+ * Created by ismet on 01/02/16.
+ */
+public class LoginResponse {
+
+    public String sessionId;
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+}
