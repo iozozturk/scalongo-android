b42c8afb1cfd508c8ac35703c902b160c3c4160c
ismet <iozozturk@gmail.com>
2016-02-11 19:43:45 +0200

    Removed obsolete model classes

--- app/src/main/java/com/ismet/durt/Models/Requests/LoginRequest.java
+++
@@ -1,7 +0,0 @@
-package com.ismet.durt.Models.Requests;
-
-/**
- * Created by ismet on 29/01/16.
- */
-public class LoginRequest {
-}
--- app/src/main/java/com/ismet/durt/Models/SignupResponse.java
+++
@@ -1,7 +0,0 @@
-package com.ismet.durt.models;
-
-/**
- * Created by ismet on 09/02/16.
- */
-public class SignupResponse {
-}
